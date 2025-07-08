d6d7a59c5caca34e987f13cbd7ae2602c7a0321c
ashim <magarashim69086@gmail.com>
2025-07-08 15:50:06 +0545

    ui:desgin finished

--- frontend/authFirst/src/Components/SignUp.jsx
+++ frontend/authFirst/src/Components/SignUp.jsx
@@ -8,32 +8,48 @@ function SignUp() {
   console.log(username);
 
   return (
-    <div className=" flex h-full shadow-2xl border-gray-200 m-27">
-      <div className="w-1/2">
-        <img src="	https://medicarelifeharmony.vercel.app/_next/image?url=%2Fauth.webp&w=1920&q=75" />
+    <div className=" flex h-screen  w-screen  justify-center   items-center ">
+      <div className="flex w-3/4 h-3/4 shadow-2xl bg-gray-200 rounded-lg overflow-hidden">
+        <div className=" object-cover  md:w-1/2">
+          <img
+            src="	https://medicarelifeharmony.vercel.app/_next/image?url=%2Fauth.webp&w=1920&q=75"
+            className="h-full"
+          />
+        </div>
+
+        <div className=" w-1/2 ">
+          <form className="tracking-wide flex flex-col justify-center items-center h-full    space-y-2  ">
+            <div className="space-x-2 flex-col flex">
+              <label>User name</label>
+              <input
+                type="text"
+                className="border-2 border-black rounded-md p-1"
+                onChange={(e) => SetUsername(e.target.value)}
+              />
+            </div>
+
+            <div className="space-x-2 flex-col flex">
+              <label>Gmail</label>
+              <input
+                type="text"
+                className="border-2 border-black rounded-md p-1"
+                onChange={(e) => SetUsername(e.target.value)}
+              />
+            </div>
+
+            <div className="space-x-2 flex-col flex">
+              <label>Password</label>
+              <input
+                type="text"
+                className="border-2 border-black rounded-md p-1"
+                onChange={(e) => SetUsername(e.target.value)}
+              />
+            </div>
+
+            <button>Sign Up</button>
+          </form>
+        </div>
       </div>
-
-      <form className="w-1/2 tracking-wide flex flex-col justify-center items-center-safe space-y-8 content-center ">
-        <div
-          className="space-x-2 "
-          onChange={(e) => SetUsername(e.target.value)}
-        >
-          <label>User name</label>
-          <input type="text" className="border-2 border-black rounded-md p-1" />
-        </div>
-
-        <div className="space-x-2">
-          <label>Gmail</label>
-          <input type="text" className="border-2 border-black rounded-md" />
-        </div>
-
-        <div className="space-x-2">
-          <label>Password</label>
-          <input type="text" className="border-2 border-black rounded-md" />
-        </div>
-
-        <button>Sign Up</button>
-      </form>
     </div>
   );
 }
