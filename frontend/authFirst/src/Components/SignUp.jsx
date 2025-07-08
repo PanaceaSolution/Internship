633546b1a3631d5fdd36958b044dca5be8529947
ashim <magarashim69086@gmail.com>
2025-07-08 13:04:42 +0545

    folder : basic files

---
+++ frontend/authFirst/src/Components/SignUp.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function SignUp() {
+  return (
+    <>
+          <div className="bg-yellow-200">
+              
+              hello there
+        <div></div>
+        <div></div>
+      </div>
+    </>
+  );
+}
+
+export default SignUp;
