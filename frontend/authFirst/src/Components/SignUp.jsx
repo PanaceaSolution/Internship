acfe21c2efe5c741e28eace47d5580b4c787bb0d
ashim <magarashim69086@gmail.com>
2025-07-08 14:17:20 +0545

    file:signup ui implementation

--- frontend/authFirst/src/Components/SignUp.jsx
+++ frontend/authFirst/src/Components/SignUp.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 
 function SignUp() {
-
   const [email, SetEmail] = useState("");
   const [password, Setpassword] = useState();
   const [username, SetUsername] = useState();
 
-  console.log(username)
+  console.log(username);
+
   return (
     <div className=" flex h-full shadow-2xl border-gray-200 m-27">
       <div className="w-1/2">
@@ -14,9 +14,11 @@ function SignUp() {
       </div>
 
       <form className="w-1/2 tracking-wide flex flex-col justify-center items-center-safe space-y-8 content-center ">
-        
-        <div className="space-x-2 " onChange={(e)=>SetUsername(e.target.value)}>
-          <label >User name</label>
+        <div
+          className="space-x-2 "
+          onChange={(e) => SetUsername(e.target.value)}
+        >
+          <label>User name</label>
           <input type="text" className="border-2 border-black rounded-md p-1" />
         </div>
 
@@ -30,7 +32,7 @@ function SignUp() {
           <input type="text" className="border-2 border-black rounded-md" />
         </div>
 
-        <button >Sign Up</button>
+        <button>Sign Up</button>
       </form>
     </div>
   );
