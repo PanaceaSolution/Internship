c7396569c0cd5922544fcc9017699b24e1c8f174
yogendrabaskota <yogendrabaskota18@gmail.com>
2025-07-09 11:53:52 +0545

    auth: bcrypt completed

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,63 +1,66 @@
 import User from "../models/userModel.js"
+import bcrypt from 'bcryptjs'
 
-class UserController{
-    async registerUser (req,res){
-        const {name, email, password} = req.body
-        if(!name || !email || !password){
-            res.status(400).json({
-                message: "please provide name, email and password"
-            })
-            return 
-        }
-        const existingUser = await User.find({email})
-        if(existingUser.length > 0){
-            res.status(409).json({
-                message: "User with this email already exists"
-            })
-            return
-        }
-        await User.create({
-            name,
-            email,
-            password
+class UserController {
+  async registerUser(req, res) {
+    try {
+      const { name, email, password } = req.body;
+      if (!name || !email || !password) {
+        res.status(400).json({
+          message: "please provide name, email and password",
         })
-        res.status(201).json({
-            message: "User created successfully"
+        return
+      }
+      const existingUser = await User.find({ email });
+      if (existingUser.length > 0) {
+        res.status(400).json({
+          message: "User with this email already exists",
         })
+        return
+      }
+      await User.create({
+        name,
+        email,
+        password: bcrypt.hashSync(password, 10),
+      })
+      res.status(201).json({
+        message: "User created successfully",
+      })
 
-    }
-
-    async loginUser(req, res){
-        const {email, password} = req.body
-        if(!email || !password){
-            res.status(400).json({
-                message: "Please provide email and password"
-            })
-            return 
-        }
-        const user = await User.find({email})
-        if(user.length == 0){
-            res.status(404).json({
-                message: "This email is not registered"
-            })
-            return 
-        }
-        // console.log(user[0].password)
-        if(user[0].password != password){
-            res.status(401).json({
-                message: "Invalid password"
-            })
-            return 
-        }
-        res.status(200).json({
-            message: "Login successful",
-            user
-        })
-
-
-    }
-
+    } catch (error) {
+      console.log(error)
     
+    }
+  }
+
+  async loginUser(req, res) {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400).json({
+        message: "Please provide email and password",
+      });
+      return;
+    }
+    const user = await User.find({ email });
+    if (user.length == 0) {
+      res.status(404).json({
+        message: "This email is not registered",
+      });
+      return;
+    }
+
+    const isMatched = bcrypt.compareSync(password, user[0].password);
+    if (isMatched) {
+      res.status(200).json({
+        message: "Login successful",
+        user,
+      });
+    } else {
+      res.status(400).json({
+        message: "Invalid password!!!",
+      });
+    }
+  }
 }
 
 export default new UserController()
