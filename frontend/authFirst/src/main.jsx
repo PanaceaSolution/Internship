3e4745236c34a8d90ce629dbcb10af4dd80e6dcb
Shubham Shrestha <shrestha.subham.60@gmail.com>
2025-07-10 10:47:56 +0545

    Setup:AppandMain.jsxIssueresolved

--- frontend/authFirst/src/main.jsx
+++ frontend/authFirst/src/main.jsx
@@ -1,44 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client"
 import "./index.css";
-import SignUp from "./Components/SignUp.jsx";
-import { Toaster } from "react-hot-toast";
-import Authtest from "./Components/authtest.jsx";
-import SignIn from "./Components/SignIn.jsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Toaster
-        position="bottom-center"
-        reverseOrder={false}
-        gutter={8}
-        containerClassName=""
-        containerStyle={{}}
-        toastOptions={{
-          className: "",
-          duration: 5000,
-          removeDelay: 1000,
-          style: {
-            background: "#363636",
-            color: "#fff",
-          },
-
-          success: {
-            duration: 3000,
-            iconTheme: {
-              primary: "green",
-              secondary: "black",
-            },
-          },
-        }}
-      />
-      <Routes>
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/secure" element={<Authtest />} />
-      </Routes>
-    </BrowserRouter>
+  <App />
   </StrictMode>
 );
